cdc84eb56a365c75993060c06ff719494fedbfbf
RevX <127177436+revx0012@users.noreply.github.com>
2023-04-26 22:04:03 +0300

    Create revhub.lua

---
+++ revhub.lua
@@ -0,0 +1,87 @@
+local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
+
+
+local Window = Library.CreateLib("Rev Hub", "GrapeTheme")
+local Tab = Window:NewTab("Scripts")
+local Section = Tab:NewSection("Script")
+Section:NewButton("Infinite yield", "infinite yield fe", function()
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()
+end)
+Section:NewButton("chat bypass v2", "chat bypass version 2",function()
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/vqmpjay/scripts/main/vadriftsloader.lua'),true))()
+end)    
+Section:NewButton("better bypasser chat", "bypass chat",function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/synnyyy/synergy/additional/betterbypasser",true))()
+end)
+
+Section:NewButton("Fling GUI", "fling gui",function()
+    loadstring(game:HttpGet('https://pastebin.com/raw/r97d7dS0', true))()
+end)
+
+Section:NewButton("keyboard", "keyboard script on mobile",function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/advxzivhsjjdhxhsidifvsh/mobkeyboard/main/main.txt", true))() 
+end)
+
+Section:NewButton("fly gui v3", "fly gui script",function()
+    loadstring(game:HttpGet(('https://pastebin.com/raw/QGLvkvU7'),true))()
+end)
+
+Section:NewButton("vfly GUI", "vehicle fly gui",function()
+    loadstring(game:HttpGet('https://pastebin.com/raw/4vu55wTN'))()
+end)
+
+Section:NewButton("Chat bypasser v1", "chat bypass version 1",function()
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/vqmpjay/chatbypasser/main/chatbypasserv1'),true))()
+end) 
+
+
+local Tab2 = Window:NewTab("Other Things")
+local SectionSpeed = Tab2:NewSection("Speed")
+
+
+SectionSpeed:NewSlider("Speed", "Boost Your Speed!", 250, 16, function(v)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = v
+end)local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
+
+
+local Window = Library.CreateLib("Rev Hub", "GrapeTheme")
+local Tab = Window:NewTab("Scripts")
+local Section = Tab:NewSection("Script")
+Section:NewButton("Infinite yield", "infinite yield fe", function()
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()
+end)
+Section:NewButton("chat bypass v2", "chat bypass version 2",function()
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/vqmpjay/scripts/main/vadriftsloader.lua'),true))()
+end)    
+Section:NewButton("better bypasser chat", "bypass chat",function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/synnyyy/synergy/additional/betterbypasser",true))()
+end)
+
+Section:NewButton("Fling GUI", "fling gui",function()
+    loadstring(game:HttpGet('https://pastebin.com/raw/r97d7dS0', true))()
+end)
+
+Section:NewButton("keyboard", "keyboard script on mobile",function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/advxzivhsjjdhxhsidifvsh/mobkeyboard/main/main.txt", true))() 
+end)
+
+Section:NewButton("fly gui v3", "fly gui script",function()
+    loadstring(game:HttpGet(('https://pastebin.com/raw/QGLvkvU7'),true))()
+end)
+
+Section:NewButton("vfly GUI", "vehicle fly gui",function()
+    loadstring(game:HttpGet('https://pastebin.com/raw/4vu55wTN'))()
+end)
+
+Section:NewButton("Chat bypasser v1", "chat bypass version 1",function()
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/vqmpjay/chatbypasser/main/chatbypasserv1'),true))()
+end) 
+
+
+local Tab2 = Window:NewTab("Other Things")
+local SectionSpeed = Tab2:NewSection("Speed")
+
+
+SectionSpeed:NewSlider("Speed", "Boost Your Speed!", 250, 16, function(v)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = v
+end)
