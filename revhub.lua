f2289ff8bc5086f82ad1a62646f3a78a8e7bb54a
RevX <127177436+revx0012@users.noreply.github.com>
2023-04-27 09:06:52 +0000

    Update revhub.lua

--- revhub.lua
+++ revhub.lua
@@ -1,70 +1,52 @@
- local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
+local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
 
+-- The hub configuration
 local Window = Library.CreateLib("Rev Hub", "GrapeTheme")
-
+-- Scripts Tab
 local Tab = Window:NewTab("Scripts")
-
 local Section = Tab:NewSection("Script")
-
 Section:NewButton("Infinite yield", "infinite yield fe", function()
-
     loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()
-
 end)
-
 Section:NewButton("chat bypass v2", "chat bypass version 2",function()
-
     loadstring(game:HttpGet(('https://raw.githubusercontent.com/vqmpjay/scripts/main/vadriftsloader.lua'),true))()
-
 end)    
-
 Section:NewButton("better bypasser chat", "bypass chat",function()
-
     loadstring(game:HttpGet("https://raw.githubusercontent.com/synnyyy/synergy/additional/betterbypasser",true))()
-
 end)
 
 Section:NewButton("Fling GUI", "fling gui",function()
-
     loadstring(game:HttpGet('https://pastebin.com/raw/r97d7dS0', true))()
-
 end)
 
 Section:NewButton("keyboard", "keyboard script on mobile",function()
-
     loadstring(game:HttpGet("https://raw.githubusercontent.com/advxzivhsjjdhxhsidifvsh/mobkeyboard/main/main.txt", true))() 
-
 end)
 
 Section:NewButton("fly gui v3", "fly gui script",function()
-
     loadstring(game:HttpGet(('https://pastebin.com/raw/QGLvkvU7'),true))()
-
 end)
 
 Section:NewButton("vfly GUI", "vehicle fly gui",function()
-
     loadstring(game:HttpGet('https://pastebin.com/raw/4vu55wTN'))()
-
 end)
 
 Section:NewButton("Chat bypasser v1", "chat bypass version 1",function()
-
     loadstring(game:HttpGet(('https://raw.githubusercontent.com/vqmpjay/chatbypasser/main/chatbypasserv1'),true))()
-
 end) 
 
+-- Other Things Tab
 local Tab2 = Window:NewTab("Other Things")
-
 local SectionSpeed = Tab2:NewSection("Speed")
 
-SectionSpeed:NewSlider("Speed", "Boost Your Speed!", 250, 16, function(v)
 
+SectionSpeed:NewSlider("Speed", "Boost Your Speed!", 300, 16, function(v)
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = v
-
 end)
 
+-- Tab info
 
-
-
-
+local SectionInfo = Tab2:NewSection("Info")
+SectionInfo:NewButton("Info", "yeah, it will say who made the hub but in chat.", function()
+    game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("This hub was made by ron144", "All")
+end)
