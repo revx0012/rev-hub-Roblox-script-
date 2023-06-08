1a60c45f856a23a0b73e0f001f0545185b9e471b
RevX <127177436+revx0012@users.noreply.github.com>
2023-06-08 18:35:41 +0000

    Update

--- revhub.lua
+++ revhub.lua
@@ -38,10 +38,6 @@ Section:NewButton("Infinite zoom", "makes your camera zoom infinite",function()
     game.Players.LocalPlayer.CameraMaxZoomDistance = math.huge
 end)
 
-Section:NewButton("Hydrogen Custom", "nothing much about it.",function() loadstring(game:HttpGet(('https://raw.githubusercontent.com/AZYsGithub/chillz-workshop/main/HydrogenCustom'),true))()
-end)
-
-
 
 
 -- Other Things Tab
@@ -56,17 +52,21 @@ end)
 -- section info
 
 local SectionInfo = Tab2:NewSection("Info")
+
 SectionInfo:NewButton("Info", "yeah, it will say who made the hub.", function()
   print('this script was made by ron144, i hope you enjoy the script!!')
  game:GetService("StarterGui"):SetCore("DevConsoleVisible", true)
 end)
 
 -- Arceus X Tab
-local Tab = Window:NewTab("Arceus X")
-local arceusx Tab:NewSection("Scripts of Arceus X")
+local Tab3 = Window:NewTab("Arceus X")
+local ArceusX Tab3:NewSection("Scripts of Arceus X")
 
-arceusx:NewButton("Arceus X Aimbot", "aim bot from arceus x",function() loadstring(game:HttpGet(('https://raw.githubusercontent.com/AZYsGithub/chillz-workshop/main/Arceus%20Aimbot.lua'),true))()
-end) 
 
-arceusx:NewButton("Arceus X V3", "arceus x",function() loadstring(game:HttpGet(('https://raw.githubusercontent.com/AZYsGithub/chillz-workshop/main/Arceus%20X%20V3'),true))()
+ArceusX:NewButton("Arceus X Aimbot", "aim bot from arceus x",function()
+ loadstring(game:HttpGet('https://raw.githubusercontent.com/AZYsGithub/chillz-workshop/main/Arceus%20X%20V3'))()
+end)
+
+ArceusX:NewButton("Arceus X V3", "arceus x",function()
+ loadstring(game:HttpGet('https://raw.githubusercontent.com/AZYsGithub/chillz-workshop/main/Arceus%20X%20V3'))()
 end)
